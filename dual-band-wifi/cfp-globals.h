2d69723bb5f17c3e3f16c66e9a496f42ccd05e1a
sharan-naribole <sn23@rice.edu>
2017-10-18 18:00:11 -0500

    Added cfp-globals.h

---
+++ dual-band-wifi/cfp-globals.h
@@ -0,0 +1,21 @@
+#ifndef CFPGLOBALS_H
+#define CFPGLOBALS_H
+
+// List of all global variables to be used in CFP Period
+
+double SIFS = 16;
+double slotTime = 9;
+double PIFS = 25;
+double DIFS = 34;
+double PHYHead = 20;
+double MACHead = 20;
+
+//double pollTxTime = 110;
+//double NullTxTime = 40;
+//double ACKTxTime = 40;
+
+double pollSize = 83;
+double ACKSize = 83;
+double ACKTimeout = 60;
+
+#endif
