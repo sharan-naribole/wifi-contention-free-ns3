0c7fb0ecfab65ccd6717616c36bc0d0f55c1211c
sharan-naribole <sn23@rice.edu>
2017-10-26 15:57:54 -0500

    AbortReception Model for 802.11 CFP completed; RandomDiscPositionAllocator installed

--- 802.11-cfp/cfp-globals.h
+++ 802.11-cfp/cfp-globals.h
@@ -7,15 +7,14 @@ double SIFS = 16;
 double slotTime = 9;
 double PIFS = 25;
 double DIFS = 34;
-double PHYHead = 20;
-double MACHead = 20;
+double decodeDelay = 1;
+double preambleDetection = 4;
 
-//double pollTxTime = 110;
-//double NullTxTime = 40;
-//double ACKTxTime = 40;
+double pollTxTime = 96; // magic number for OFDM 18 Mbps and pollSize = 83
+double NullTxTime = 40;
+double ACKTxTime = 40;
 
 double pollSize = 83;
-double ACKSize = 83;
-double ACKTimeout = 60;
+double ACKSize = 20;
 
 #endif
